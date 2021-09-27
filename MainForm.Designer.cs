a41bce6964df8dc697340603caa3128a9eeca727
hoaffffff <hoadaknong101@gmail.com>
2021-09-27 16:55:31 +0700

    Thêm button

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,13 +29,24 @@ namespace management_store
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 191);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1084, 561);
+            this.Controls.Add(this.button1);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +54,8 @@ namespace management_store
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
