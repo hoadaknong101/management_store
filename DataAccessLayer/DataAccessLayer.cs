0657e278bcad5ce9ae132fb24f0a3c171995ffec
Pham Hoa <phamdinhquochoa101@gmail.com>
2022-11-09 11:33:31 +0700

    unit test

--- DataAccessLayer/DataAccessLayer.cs
+++ DataAccessLayer/DataAccessLayer.cs
@@ -7,7 +7,7 @@ namespace management_store
     class DataAccessLayer
     {
         // Hoa
-        //private string connectionString = "Data Source=desktop-m8qf5it\\SQLEXPRESS;Initial Catalog=CNPM_PHONG_PHAM;Integrated Security=True;MultipleActiveResultSets=true";
+        private string connectionString = "Data Source=desktop-m8qf5it\\SQLEXPRESS;Initial Catalog=CNPM_PHONG_PHAM;Integrated Security=True;MultipleActiveResultSets=true";
 
         // Hao
         //private string connectionString = "Data Source=localhost\SQLEXPRESS;Initial Catalog=CNPM_PHONG_PHAM;Integrated Security=True;MultipleActiveResultSets=true"
@@ -16,7 +16,7 @@ namespace management_store
         //private string connectionString = "Data Source=PhiVo-Z1704;Initial Catalog=CNPM_PHONG_PHAM;Integrated Security=True;MultipleActiveResultSets=true";
 
         //Thang
-        private string connectionString = "Data Source=DESKTOP-N0B1KM6\\SQLEXPRESS;Initial Catalog=CNPM_PHONG_PHAM;Integrated Security=True;MultipleActiveResultSets=true";
+        //private string connectionString = "Data Source=DESKTOP-N0B1KM6\\SQLEXPRESS;Initial Catalog=CNPM_PHONG_PHAM;Integrated Security=True;MultipleActiveResultSets=true";
 
         public SqlConnection connection;
         public SqlCommand cmd;
